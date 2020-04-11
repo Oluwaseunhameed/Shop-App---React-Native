b188993dd26b37f71e76ca8157b058ae53acca57
Oluwaseunhameed <hameedoluwaseun@gmail.com>
2020-04-11 10:15:40 +0100

    ProductsOverviewScreen initial layout & styling

---
+++ navigation/ShopNavigator.js
@@ -0,0 +1,35 @@
+import React from "react";
+import { Platform } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createDrawerNavigator } from "@react-navigation/drawer";
+import { createStackNavigator } from "@react-navigation/stack";
+import { Ionicons } from "@expo/vector-icons";
+
+import ProductsOverviewScreen from "../screens/shop/ProductsOverviewScreen";
+import Colors from "../constants/Colors";
+
+const defaultHeaderSettings = {
+  headerStyle: {
+    backgroundColor: Platform.OS === "android" ? Colors.primary : "",
+  },
+  headerTintColor: Platform.OS === "android" ? "white" : Colors.primary,
+  headerTitleAlign: "center",
+};
+
+const ProductsNavigator = createStackNavigator();
+
+const ShopNavigator = () => {
+  return (
+    <NavigationContainer>
+      <ProductsNavigator.Navigator screenOptions={defaultHeaderSettings}>
+        <ProductsNavigator.Screen
+          name="All Products"
+          component={ProductsOverviewScreen}
+          options={{ title: "All Products" }}
+        />
+      </ProductsNavigator.Navigator>
+    </NavigationContainer>
+  );
+};
+
+export default ShopNavigator;
