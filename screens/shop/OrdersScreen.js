2e4757702d2637d6578d2b1c554467fc46bc04ef
Oluwaseunhameed <hameedoluwaseun@gmail.com>
2020-04-17 18:05:38 +0100

    Order screen added

--- screens/shop/OrdersScreen.js
+++ screens/shop/OrdersScreen.js
@@ -4,6 +4,7 @@ import { useSelector } from "react-redux";
 import { HeaderButtons, Item } from "react-navigation-header-buttons";
 
 import HeaderButton from "../../components/UI/HeaderButton";
+import OrderItem from "../../components/shop/OrderItem";
 
 const OrdersScreen = (props) => {
   const { navigation } = props;
@@ -29,7 +30,13 @@ const OrdersScreen = (props) => {
     <FlatList
       data={orders}
       keyExtractor={(item) => item.id}
-      renderItem={(itemData) => <Text>{itemData.item.totalAmount}</Text>}
+      renderItem={(itemData) => (
+        <OrderItem
+          amount={itemData.item.totalAmount}
+          date={itemData.item.readableDate}
+          items={itemData.item.items}
+        />
+      )}
     />
   );
 };
